cc3ed7f42a34d6b027d24a7ddc8781667574c4e9
Constantus <38108928+Constantus@users.noreply.github.com>
2021-06-05 11:32:08 -0400

    update sqlpractice.py

---
+++ sqlPractice.py
@@ -0,0 +1,16 @@
+# sqlPractice.py
+# Author: Matthew Talamantes
+# Date: June 5, 2021
+# Description: A simple program to practice SQLAlchemy and use of databases
+
+from sqlalchemy import (
+    create_engine,
+    text,
+)
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"), [{"x": 1, "y": 1}, {"x": 2, "y": 4}])
+    conn.commit()
